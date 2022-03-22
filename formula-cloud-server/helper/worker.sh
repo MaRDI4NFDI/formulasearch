8a1641c1a3706d2c95346c40941a1b8aee54a157
Stegmujo <johannesst@gmx.de>
2022-03-22 14:22:03 +0100

    Deletions of submodule content.

--- formula-cloud-server/helper/worker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-arg="$*"
-echo "[$$] start indexing $arg"
-
-dbname="${arg##*/}"
-programout=$(basexclient -V -Uadmin -Padmin -c "CREATE DB $dbname $arg")
-echo "[$$] $programout"
-echo "[$$] finished indexing."
--- formula-cloud-server/src/main/java/com/formulasearchengine/formulacloud/beans/IMathMergeFunction.java
+++
@@ -1,8 +0,0 @@
-package com.formulasearchengine.formulacloud.beans;
-
-/**
- * @author Andre Greiner-Petter
- */
-public interface IMathMergeFunction {
-    double calculate(double... values);
-}
